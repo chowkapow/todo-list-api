6ea625f13098926be10d0a1dd1232286dbdbb6d4
Eric Chow <eric.zjp.chow@gmail.com>
2024-04-18 19:34:35 +0800

    Add postgres, routes for get all, get by id, post new todo

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import bodyParser from "body-parser";
 import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 
+dotenv.config();
+import pool from "./db";
+
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(bodyParser.json());
@@ -15,4 +20,57 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello World");
 });
 
-app.listen(3000);
+app.get("/todos", async (req: Request, res: Response) => {
+  try {
+    const { rows } = await pool.query("SELECT * from todos");
+    res.json(rows);
+  } catch (error) {
+    console.error("Error executing SQL query to get all todos: " + error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.get("/todos/:id", async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const { rows } = await pool.query("SELECT * from todos WHERE id = $1", [
+      id,
+    ]);
+    if (rows.length === 0) {
+      return res.status(404).json({ error: "Todo item not found" });
+    }
+    res.json(rows[0]);
+  } catch (error) {
+    console.error("Error executing SQL query to get single todo: " + error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.post("/todos", async (req: Request, res: Response) => {
+  try {
+    const { title, description } = req.body;
+
+    const now = new Date();
+
+    const queryText = `
+      INSERT INTO todos (title, description, completed, "createdAt", "updatedAt")
+      VALUES ($1, $2, $3, $4, $5)
+      RETURNING *
+    `;
+
+    const { rows } = await pool.query(queryText, [
+      title,
+      description,
+      false,
+      now,
+      now,
+    ]);
+
+    res.status(201).json(rows[0]);
+  } catch (error) {
+    console.error("Error executing SQL query to post todo: " + error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.listen(PORT);
