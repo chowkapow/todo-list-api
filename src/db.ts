36f2313b29b9b061927adcf9ace26fbd20bee875
Eric Chow <eric.zjp.chow@gmail.com>
2024-04-23 16:01:52 +0800

    Add PUT route, refactor updatedAt to db trigger

--- src/db.ts
+++ src/db.ts
@@ -1,7 +1,10 @@
+import dotenv from "dotenv";
 import { Pool } from "pg";
 
+dotenv.config();
+
 const pool = new Pool({
-  user: process.env.DB_USER,
+  user: process.env.DB_USERNAME,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
--- src/routes/todoRoutes.ts
+++ src/routes/todoRoutes.ts
@@ -33,23 +33,60 @@ router.post("/", async (req: Request, res: Response) => {
   try {
     const { title, description } = req.body;
 
-    const now = new Date();
-
     const queryText = `
-      INSERT INTO todos (title, description, completed, "createdAt", "updatedAt")
-      VALUES ($1, $2, $3, $4, $5)
+      INSERT INTO todos (title, description)
+      VALUES ($1, $2)
       RETURNING *
     `;
 
-    const { rows } = await pool.query(queryText, [
-      title,
-      description,
-      false,
-      now,
-      now,
-    ]);
+    const { rows } = await pool.query(queryText, [title, description]);
 
     res.status(201).json(rows[0]);
+  } catch (error) {
+    console.error("Error executing SQL query to post todo: " + error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+router.put("/:todoId", async (req: Request, res: Response) => {
+  try {
+    const { todoId } = req.params;
+    const { title, description, completed } = req.body;
+
+    const values = [];
+    const placeholders = [];
+
+    if (title !== undefined) {
+      values.push(title);
+      placeholders.push(`title = $${values.length}`);
+    }
+
+    if (description !== undefined) {
+      values.push(description);
+      placeholders.push(`description = $${values.length}`);
+    }
+
+    if (completed !== undefined) {
+      values.push(completed);
+      placeholders.push(`completed = $${values.length}`);
+    }
+
+    if (values.length === 0) {
+      return res.status(400).json({ error: "No fields provided for update" });
+    }
+
+    const setClause = placeholders.join(", ");
+
+    const queryText = `UPDATE todos SET ${setClause} WHERE id = $${values.length + 1} RETURNING *`;
+
+    const { rows } = await pool.query(queryText, [...values, todoId]);
+
+    if (rows.length === 0) {
+      return res.status(404).json({ error: "Todo item not found" });
+    }
+
+    // If the Todo item is successfully updated, return it as a response
+    res.json(rows[0]);
   } catch (error) {
     console.error("Error executing SQL query to post todo: " + error);
     res.status(500).json({ error: "Internal server error" });
